cf7511c1639a4092a10991bc3416f4da51c575e8
Tomer <meskintomer@gmail.com>
2023-11-03 11:45:41 +0200

    Some touches

--- baseClientSide.py
+++ baseClientSide.py
@@ -1,13 +1,23 @@
 
 import socket
 MAX_PACKET = 1024
+COMMANDS = ['TIME', 'NAME', 'RAND', 'EXIT']
+
+def command():
+    com = input("Enter the desired command")
+    if (com in COMMANDS):
+        return com
+    else:
+        return None
+
 
 def main():
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         my_socket.connect(('127.0.0.1', 1729))
-        my_socket.send(input('Choose a command: ').encode())
+        my_socket.send(command().encode())
         response = my_socket.recv(MAX_PACKET).decode()
+        print(response)
     except socket.error as err:
         print("recevied socket error " + str(err))
     finally:
--- baseServerSide.py
+++ baseServerSide.py
@@ -33,11 +33,11 @@ def rand():
     :return: random_num - the random num
     """
     random_num = random.randint(1, 11)
-    return random_num
+    return str(random_num)
 
 
 def main():
-    server_socket = socket.socket(socket.AF_INET, socket.STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         server_socket.bind(('0.0.0.0', 1729))
         server_socket.listen(QUEUE_LEN)
@@ -45,14 +45,17 @@ def main():
             client_socket, client_address = server_socket.accept()
             try:
                 request = client_socket.recv(MAX_PACKET).decode()
+                print('Received the command: ' + request)
                 if(request == 'TIME'):
                     client_socket.send(time().encode())
                 elif(request == 'NAME'):
                     client_socket.send(NAME.encode())
                 elif(request == 'RAND'):
                     client_socket.send(rand().encode())
-                else:
+                elif(request == 'EXIT'):
                     return
+                else:
+                    client_socket.send('Invalid command'.encode())
             except socket.error as err:
                 print('received socket error on client socket' + str(err))
             finally:
