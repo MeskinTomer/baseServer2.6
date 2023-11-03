4765bb43e908d5f6b7da1e9832dced41ad456e1d
Tomer <meskintomer@gmail.com>
2023-11-03 09:57:43 +0200

    Adding the 4 functions

--- baseServerSide.py
+++ baseServerSide.py
@@ -1,20 +1,57 @@
-import sys
+
+
 import logging
 import socket
+import random
 from datetime import datetime
+
 
 MAX_PACKET = 4
 QUEUE_LEN = 1
+NAME = "my name is jef"
+
+
+def time():
+    """
+    Returns the current time
+    :param time_and_date: gets the current time and date
+    :type param time_and_date: string
+    :param current_time: the current time seperated from the date
+    :type paran current_time: string
+    :return: current_time - the time seperated into hours, minutes and seconds
+    """
+    time_and_date = datetime.now()
+    current_time = time_and_date.strftime('%H:%M:%S')
+    return current_time
+
+
+def rand():
+    """
+    Generates a random number between 1 and 10
+    :param random_num: the generated random number
+    :type param random_num: int
+    :return: random_num - the random num
+    """
+    random_num = random.randint(1, 11)
+    return random_num
 
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.STREAM)
-    try:
+     try:
         server_socket.bind(('0.0.0.0', 1729))
         server_socket.listen(QUEUE_LEN)
         client_socket, client_address = server_socket.accept()
         try:
             request = client_socket.recv(MAX_PACKET).decode()
+            if(request == 'TIME'):
+                client_socket.send(time().encode())
+            elif(request == 'NAME'):
+                client_socket.send(NAME.encode())
+            elif(request == 'RAND'):
+                client_socket.send(rand().encode())
+            else:
+
         except socket.error as err:
             print('received socket error on client socket' + str(err))
         finally:
